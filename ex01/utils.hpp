f7c4c528611e11f13f4f6b547eda65a2a3ef4180
Jonathan Gasparoly <jgasparo@fu-r4-p1.s19.be>
2024-10-04 14:21:06 +0200

    fix: macro maj

--- ex01/utils.hpp
+++ ex01/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: jgasparo <jgasparo@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 09:39:12 by jgasparo          #+#    #+#             */
-/*   Updated: 2024/10/02 11:17:34 by jgasparo         ###   ########.fr       */
+/*   Updated: 2024/10/04 13:56:12 by jgasparo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,25 @@
 #define EMPTY 			""
 #define	PIPE			"|"
 #define	D_DOT			" : "
-#define	SUCCESS			"Contact successfully added"
+#define	SUCCESS			"CONTACT SUCCESSFULLY ADDED"
 #define	CLEAR			"clear"
 #define	ADD				"ADD"
 #define	SEARCH			"SEARCH"
 #define	EXIT			"EXIT"
 #define	ALL				"ALL"
-#define	ENTER			"Enter "
-#define	FILL			"Fill please ..."
-#define	UNRECOGNIZED	"Unrecognized Option, Try Again !"
-#define	CTRL_D			"Input ^D detected. Exiting..."
+#define	ENTER			"ENTER "
+#define	FILL			"FILL PLEASE ..."
+#define	UNRECOGNIZED	"UNRECOGNIZED OPTION, TRY AGAIN !"
+#define	CTRL_D			"INPUT ^D DETECTED. EXITING..."
 #define	DESTRUCTION		"PHONEBOOK WILL SELF-DESTRUCT IN 3 ... 2 ... 1 ..."
 #define	WELCOME			"WELCOME IN THE PHONEBOOK"
 #define	CHOOSE_OPTION	"CHOOSE YOUR OPTION : ADD / SEARCH / EXIT"
-#define	NO_NUMBER		"Input not an number, try again"
-#define	OUT_RANGE		"Input not between 1 and 8 or ALL, try again"
-#define	WHICH			"Which contact do you want to see (1-8 or ALL) ? "
-#define	NOT_FILLED		"Contact not filled"
-#define	WRONG			"Wrong index"
-#define	EMPTY_FIELD		"empty fields not accepted, try again"
+#define	NO_NUMBER		"INPUT NOT AN NUMBER, TRY AGAIN"
+#define	OUT_RANGE		"INPUT NOT BETWEEN 1 AND 8 OR ALL, TRY AGAIN"
+#define	WHICH			"WHICH CONTACT DO YOU WANT TO SEE (1-8 OR ALL) ? "
+#define	NOT_FILLED		"CONTACT NOT FILLED"
+#define	WRONG			"WRONG INDEX"
+#define	EMPTY_FIELD		"EMPTY FIELDS NOT ACCEPTED, TRY AGAIN"
 bool	safeGetline(std::string &str);
 
 #endif
