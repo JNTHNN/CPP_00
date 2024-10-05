98e26fb64fe9805d2f882aaf1efdef6c057594c8
JNTHNN <j.gasparoly@gmail.com>
2024-10-05 21:31:37 +0200

    fix: ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: jgasparo <jgasparo@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 09:35:06 by jgasparo          #+#    #+#             */
-/*   Updated: 2024/09/23 11:18:23 by jgasparo         ###   ########.fr       */
+/*   Updated: 2024/10/05 21:18:37 by jgasparo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,11 @@ int main (int argc, char **argv)
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int j = 1; j < argc; j++)
-		for (size_t i = 0; i < strlen(argv[j]); i++)
+	{
+		std::string arg = std::string(argv[j]);
+		for (size_t i = 0; i < arg.length(); i++)
 			std::cout << std::toupper(argv[j][i], loc);
+	}
 	std::cout << std::endl;
 	return 0;
 }
